bd5642222433a40008c0ac660ecda82fb61e4f4c
Riptide <ripta@rp.wv.cc.cmu.edu>
2008-11-25 12:14:35 -0500

    Link to the chat demo

--- app/controllers/chat_demos.rb
+++ app/controllers/chat_demos.rb
@@ -1,30 +1,30 @@
-class ChatDemos < Application
+class MerbJuggernaut::ChatDemos < MerbJuggernaut::Application
   # provides :xml, :yaml, :js
-
+  
   def index
     @chat_demos = ChatDemo.all
     display @chat_demos
   end
-
+  
   def show(id)
     @chat_demo = ChatDemo.get(id)
     raise NotFound unless @chat_demo
     display @chat_demo
   end
-
+  
   def new
     only_provides :html
     @chat_demo = ChatDemo.new
     display @chat_demo
   end
-
+  
   def edit(id)
     only_provides :html
     @chat_demo = ChatDemo.get(id)
     raise NotFound unless @chat_demo
     display @chat_demo
   end
-
+  
   def create(chat_demo)
     @chat_demo = ChatDemo.new(chat_demo)
     if @chat_demo.save
@@ -34,7 +34,7 @@ class ChatDemos < Application
       render :new
     end
   end
-
+  
   def update(id, chat_demo)
     @chat_demo = ChatDemo.get(id)
     raise NotFound unless @chat_demo
@@ -44,7 +44,7 @@ class ChatDemos < Application
       display @chat_demo, :edit
     end
   end
-
+  
   def destroy(id)
     @chat_demo = ChatDemo.get(id)
     raise NotFound unless @chat_demo
@@ -54,5 +54,5 @@ class ChatDemos < Application
       raise InternalServerError
     end
   end
-
-end # ChatDemos
+  
+end
