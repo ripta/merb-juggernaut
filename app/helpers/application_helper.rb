8a7beaca1f5d379a11fe3e15b184d2f4a9ffcded
Riptide <ripta@rp.wv.cc.cmu.edu>
2008-12-05 11:03:50 -0500

    Add juggernaut view helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,33 @@
 module Merb
   module MerbJuggernaut
+    
     module ApplicationHelper
+      
+      def juggernaut(options = { })
+        hosts = ::MerbJuggernaut::CONFIG[:hosts].select {|h| !h[:environment] || h[:environment].to_sym == Merb.env.to_sym }
+        random_host = hosts[rand(hosts.length)]
+        options = {
+          :host                 => (random_host[:public_host] || random_host[:host]),
+          :port                 => (random_host[:public_port] || random_host[:port]),
+          :width                => '0px',
+          :height               => '0px',
+          :session_id           => session.session_id,
+          :swf_address          => "/media/juggernaut.swf",
+          :ei_swf_address       => "/media/expressinstall.swf",
+          :flash_version        => 8,
+          :flash_color          => "#fff",
+          :swf_name             => "juggernaut_flash",
+          :bridge_name          => "juggernaut",
+          :debug                => (Merb.env.to_s == 'development'),
+          :reconnect_attempts   => 3,
+          :reconnect_intervals  => 3
+        }.merge(options)
+        %Q{<script type="text/javascript" charset="utf-8">
+          jQuery(document).ready(function() {
+            jQuery.Juggernaut.initialize(#{options.to_json})
+          });
+        </script>}
+      end
       
       # @param *segments<Array[#to_s]> Path segments to append.
       #
